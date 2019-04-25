d08276349e7d7d67fc58db93242dc7e8c25d2eb6
lore0223 <loremma23@gmail.com>
2019-04-24 20:04:46 -0500

    commit para fusionar con el repo master

---
+++ src/javascript/firebase.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyDXy5eN5wzec8uCqg3ZvAn-z-hLSEP8NkI",
+    authDomain: "adamantium-64b17.firebaseapp.com",
+    databaseURL: "https://adamantium-64b17.firebaseio.com",
+    projectId: "adamantium-64b17",
+    storageBucket: "adamantium-64b17.appspot.com",
+    messagingSenderId: "727524590455"
+  };
+  firebase.initializeApp(config);
+  
